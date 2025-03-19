a55f83625a8f1eb3d62d770f83e7dc557fbbc215
clshu <clshu2020@gmail.com>
2025-03-19 17:42:37 +0800

    Create GET /hello route and ahandler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused)]
+
+use axum::{Router, response::Html, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let route_hello = Router::new().route(
+        "/hello",
+        get(|| async { Html("Hello, <strong>World!</strong>") }),
+    );
+
+    // region:    --- Start Server
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    println!("->> Listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, route_hello).await.unwrap();
+    // endregion: --- Start Server
 }
